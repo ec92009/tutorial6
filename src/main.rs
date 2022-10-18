01654d66839913c9075f6c2dc80ec2b10d8c77d7
Elie Cohen <ec92009@gmail.com>
2022-10-18 11:13:07 +0100

    explicit type conversion

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let x = 255u8; 
+    let x = 255u32; 
     let y = 10_u8;  
 
-    let z = x % y;
+    let z = x % y as u32;
     println!("Hello, world! x:{} y:{} z:{}", x, y, z);
 
 
