4a991821eab994f05475c838e102999e57bf80c3
Elie Cohen <ec92009@gmail.com>
2022-10-18 11:08:51 +0100

    divide

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x: u8 = 255; // 0..255
+    let y: u8 = 10; // 
+
+    let z = x / y;
+    println!("Hello, world! x:{} y:{} z:{}", x, y, z);
+
+
 }
