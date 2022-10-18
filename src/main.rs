96b6880e2c8cb50b4e37dd4e114bc6d1971fe029
Elie Cohen <ec92009@gmail.com>
2022-10-18 11:11:52 +0100

    alternate notation 255u8

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let x: u8 = 255; // 0..255
-    let y: u8 = 10; // 
+    let x = 255u8; 
+    let y = 10_u8;  
 
     let z = x % y;
     println!("Hello, world! x:{} y:{} z:{}", x, y, z);
