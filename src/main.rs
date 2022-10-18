6bdf70c2769be0aad4dca4d89c3003cf7ca9a391
Elie Cohen <ec92009@gmail.com>
2022-10-18 11:28:36 +0100

    explicit conversion from input to variable

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use std::io;
+
 fn main() {
     let x = i32::MAX as i64 + 1; 
     let y = 10_u8;  
 
     let z = x % y as i64;
     println!("Hello, world! x:{} y:{} z:{}", x, y, z);
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Error reading");
+    let int_input: i64 = input.trim().parse().expect("Error converting string");
+
+    println!("{} + 2 = {}", input.trim(), int_input + 2);
 
 
 }
