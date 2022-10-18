270409fb70415187bdfd4dc2055a7fa2ffac535b
Elie Cohen <ec92009@gmail.com>
2022-10-18 11:15:19 +0100

    explicit conversion again

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let x = 255u32; 
+    let x = i32::MAX as i64 + 1; 
     let y = 10_u8;  
 
-    let z = x % y as u32;
+    let z = x % y as i64;
     println!("Hello, world! x:{} y:{} z:{}", x, y, z);
 
 
